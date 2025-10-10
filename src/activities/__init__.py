983e1b49450693bf3d76ad9534b35726747584d6
Himson Wan <himnsonwan@eduroam-int-dhcp-97-56-190.ucl.ac.uk>
2025-10-10 14:10:59 +0100

    update

--- src/activities/__init__.py
+++ src/activities/__init__.py
@@ -1,11 +1,16 @@
 from pathlib import Path
 
+def activity1():
 # This script is located in the project root, so find the path to the current file and then go to the parent of that file
-project_root = Path(__file__).parent
+    project_root = Path(__file__).parent
 
 # Find the .csv file relative to the project root and join to that path the data folder and then the example.csv file
-csv_file = project_root.joinpath('data', 'paralympics_raw.csv')
+    csv_file = project_root.joinpath('data', 'paralympics_raw.csv')
 # csv_file = project_root / 'data' / 'example.csv' # this notation would also work, even though you think the '/' is only unix/macOS
 
 # Check if the file exists, this will print 'true' if it exists
-print(csv_file.exists())
+    print(csv_file.exists())
+
+def activity2():
+    data_file_csv = resources.files(data).joinpath("paralympics_raw.csv")
+    
